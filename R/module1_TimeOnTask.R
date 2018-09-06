52feef1db3c2a40b724a031e0bc5b048f9ef6e45
Denise Reis Costa <deniserc@uio.no>
2018-09-06 14:57:58 +0200

    export plot

--- R/module1_TimeOnTask.R
+++ R/module1_TimeOnTask.R
@@ -174,3 +174,30 @@ RangeTimeonTaskbyVar <- function(data, tot.var, var.group) {
   print(n.event.var, n=50)
   return(n.event.var)
 }
+
+###########################################
+##Plot ToT by var###
+###########################################
+#' Check response time by var
+#'
+#' This is a function that reports the number of students and number de actions (min-max)
+#' aggregated by a specific variable.
+#'
+#' @param  data A \code{matrix} or \code{data.frame}
+#' @param tot.var a vector with the total time. It is a \code{quo()} type.
+#' @param var.group a vector with the group variable. It is a \code{quo()} type.
+#'
+#' @return This function returns a \code{data.frame} with the number of students and number de actions (min-max)
+#' aggregated by a specific variable.
+#'
+# @examples
+# RangeNumberActionsbyVar(data = cp025q01.data, id.var = quo(NewID), var.group = quo(cnt))
+#'
+#' @export
+PlotTimeonTaskbyVar <- function(data, tot.var, performance.item, namevar, namebreaks, namelabels, namexlab, nameylab) {
+
+  data[[performance.item]] <- as.factor(data[[performance.item]])
+
+  ggplot2::ggplot(data, aes_string(x=tot.var, fill=performance.item))+ theme_minimal() +  scale_fill_grey() +
+    geom_density(alpha = 0.5)+ xlab(namexlab) + ylab(nameylab) + xlim(0, 5) + ylim(0, 1)
+}
