93b8df743f8755b5f6f2ae507ac08c5e14f6be01
Denise Reis Costa <deniserc@uio.no>
2018-09-12 17:58:27 +0200

    percentage within the categorized proficiency score

--- R/module2_SequenceOfActions.R
+++ R/module2_SequenceOfActions.R
@@ -467,7 +467,7 @@ PlotStrategybyCatPerformance <- function(data, strategy.var, categ.var, namexlab
   aggregate.data <-  data %>%
     dplyr::group_by(!!strategy.var, !!categ.var) %>%
     dplyr::summarise(n = n()) %>%
-    dplyr::ungroup() %>%
+    dplyr::group_by(!!categ.var) %>%
     dplyr::mutate(freq = 100* n / sum(n))
   
   aggregate.data <- as.data.frame(aggregate.data)
@@ -475,7 +475,7 @@ PlotStrategybyCatPerformance <- function(data, strategy.var, categ.var, namexlab
     ggplot(data=aggregate.data, aes_string(x=names(aggregate.data)[2], y=names(aggregate.data)[4], fill=names(aggregate.data)[1])) +
       geom_bar(stat="identity", position=position_dodge())+ scale_fill_grey() +
       theme(axis.text.y = element_blank()) +
-      geom_text(aes(label=paste0(round(freq,0), "%")), vjust=1.6, color="white",
+      geom_text(aes(label=paste(n, paste0(" (", round(freq,0), "%)"), sep='\n')), vjust=1.1, color="white",
                 position = position_dodge(0.9), size=3.5) +
       xlab(namexlab) + ylab(nameylab)
 }
