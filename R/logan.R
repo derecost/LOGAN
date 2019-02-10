3c8ca6340ef01ca2713df726741176a322431813
Waldir Leoncio <waldir.leoncio@gmail.com>
2019-02-10 16:37:38 +0100

    Improved formatting of ?LOGAN

--- R/logan.R
+++ R/logan.R
@@ -1,11 +1,10 @@
-#' LOGAN: Log File Analysis in International Large-scale Assessments
+#' @title LOGAN: Log File Analysis in International Large-scale Assessments
 #'
-#' Overview of the LOGAN package.
-#'
-#' This package enables users to handle the dataset cleaning for conducting
-#' specific analyses with the log files from two international educational
-#' assessments: the Programme of International Student Assessment (PISA) and the
-#' Programme for International Assessment of Adult Competencies (PIAAC).
+#' @description This package enables users to handle the dataset cleaning for
+#'   conducting specific analyses with the log files from two international
+#'   educational assessments: the Programme of International Student Assessment
+#'   (PISA) and the Programme for International Assessment of Adult Competencies
+#'   (PIAAC).
 #'
 #' @section LOGAN functions: The LOGAN functions
 #'
