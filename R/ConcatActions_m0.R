e157622cb3d24d83646120e1caac774f5a342dc6
Waldir Leoncio <waldir.leoncio@gmail.com>
2019-02-12 07:51:51 +0100

    Removed pipe and improved formatting and documentation of ConcatActions

--- R/ConcatActions_m0.R
+++ R/ConcatActions_m0.R
@@ -3,27 +3,27 @@
 #' This function allows you to concatenate event actions from diferent variables
 #' in a unique vector.
 #'
-#' @param data A \code{matrix} or \code{data.frame} where the concatenate events
-#'   are
+#' @param data A \code{matrix} or \code{data.frame} where the concatenated
+#'   events are
 #' @param concat.events a vector where all the events are listed. Each element
 #'   of this vector needs to be of a \code{quo()} type.
 #'
-#' @return This function returns a \code{data.frame} with the concatenate events
-#'   in the 'event.type' variable.
+#' @return This function returns a \code{data.frame} with the concatenated
+#'   events in the 'event.type' variable.
+#'
+#' @examples
+#' df <-
 #'
 #' @export
 ConcatActions <- function(data, concat.events) {
 
-    for(i in seq(length(concat.events))){
+    for (i in seq(length(concat.events))) {
         events <- concat.events[[i]]
-        #events <- rlang::quo(events)
-
-        if(i==1){
-            data <- data %>%
-                dplyr::mutate(event.type = !!events)
-        }else{
-            data <- data %>%
-                dplyr::mutate(event.type = paste0(event.type,"_",!!events))
+        if (i == 1) {
+            data <- dplyr::mutate(data, event.type = !!events)
+        } else {
+            data <- dplyr::mutate(data, event.type = paste0(event.type,"_",
+                                                            !!events))
         }
     }
     return(data)
