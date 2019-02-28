1b1b3ee160f33604957e6c66244b102b93687bbd
Waldir Leoncio <waldir.leoncio@gmail.com>
2019-02-28 10:52:17 +0100

    Printing behavior changed to avoid always printing

--- R/RangeNumberActionsbyVar_m0.R
+++ R/RangeNumberActionsbyVar_m0.R
@@ -1,15 +1,18 @@
 #' Check number of students and actions by var
 #'
-#' This is a function that reports the number of students and number de actions (min-max)
-#' aggregated by a specific variable.
+#' This is a function that reports the number of students and number de actions
+#' (min-max) aggregated by a specific variable.
 #'
 #' @param  data A \code{matrix} or \code{data.frame}
-#' @param id.var a vector with the individuals identification. It is a \code{quo()} type.
+#' @param id.var a vector with the individuals identification. It is a
+#'   \code{quo()} type.
 #' @param var.group a vector with the group variable. It is a \code{quo()} type.
-#' @param save.table if \code{TRUE}, will return the table printed
-#'
-#' @return This function returns a \code{data.frame} with the number of students and number de actions (min-max)
-#' aggregated by a specific variable.
+#' @param save.table if \code{TRUE}, will save the table generated as an object
+#'   of class \code{data.frame}. Otherwise, will print the table in pandoc
+#'   format, but the object will not be saved (even if the user assigns it to an object)
+#' @param print it \code{TRUE}, will print the table generated
+#' @return This function returns a \code{data.frame} with the number of students
+#'   and number de actions (min-max) aggregated by a specific variable.
 #'
 #' @export
 RangeNumberActionsbyVar <- function(data, id.var, var.group, save.table = TRUE) {
@@ -28,12 +31,12 @@ RangeNumberActionsbyVar <- function(data, id.var, var.group, save.table = TRUE) 
                          "SD" = stats::sd(n.event),
                          "Median" = stats::median(n.event),
                          "Max" = max(n.event))
-    #print(n.event.var, n=50)
     n.event.var <- as.data.frame(n.event.var)
 
-    cat("\n Summary of number of events by country - Individual level")
-    pander::pandoc.table(n.event.var,split.tables=100)
-    if(save.table == TRUE) {
+    if (save.table) {
         return(n.event.var)
+    } else {
+        cat("Summary of number of events by country - Individual level\n")
+        pander::pandoc.table(n.event.var,split.tables = 100)
     }
 }
