9468d86425d52084ce862fb0a027b3036e0d4625
Waldir Leoncio <waldir.leoncio@gmail.com>
2019-03-12 10:52:15 +0100

    Added Sweden to datasets

--- data/fullDatasets/datasetReducer.R
+++ data/fullDatasets/datasetReducer.R
@@ -1,8 +1,10 @@
 load("data/fullDatasets/cp025q01.complete.rda")
 load("data/fullDatasets/cp025q01.data.rda")
 
-cp025q01.complete <- cp025q01.complete[cp025q01.complete$CNT == "NOR", ]
+countries <- c("NOR", "SWE")
+
+cp025q01.complete <- cp025q01.complete[cp025q01.complete$CNT %in% countries, ]
 save(cp025q01.complete, file = "data/cp025q01.complete.rda", compress = "xz")
 
-cp025q01.data <- cp025q01.data[cp025q01.data$cnt == "NOR", ]
+cp025q01.data <- cp025q01.data[cp025q01.data$cnt %in% countries, ]
 save(cp025q01.data, file = "data/cp025q01.data.rda", compress = "xz")
